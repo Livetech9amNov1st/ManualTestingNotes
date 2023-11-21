41f31bc9dfaaaa68b8f2a73e220859c7e7a57317
seleniumriyaz <selenium.riyaz.shared@gmail.com>
2023-11-21 10:17:22 +0530

    Java Notes till Nov 21st

---
+++ Hello.java
@@ -0,0 +1,14 @@
+// this is a java program to print Hello Java console 
+class Hello
+{
+
+public static void main(String[] args)
+{
+
+System.out.println("Hello All... Welcome to Java ..");
+
+}
+
+
+
+}
